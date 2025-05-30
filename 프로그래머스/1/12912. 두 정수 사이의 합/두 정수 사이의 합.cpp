b93aa75c13aa45ba6a71f6636dc58743d737f9c9
gussen33 <gussen1221@gmail.com>
2025-05-30 11:41:47 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 4.04 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.cpp
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.cpp
@@ -1,11 +1,21 @@
-#include <string>
-#include <vector>
-
-using namespace std;
-
 long long solution(int a, int b) {
-    long long minVal = min(a, b);
-    long long maxVal = max(a, b);
-    long long count = maxVal - minVal + 1;
-    return (minVal + maxVal) * count / 2;
+    long long x = a;  /*a,b 가 변하지 않게 x,y로 대체(a,b는 첫번째 while문에서 바뀔경우 뒤에서 사용이 힘들어짐)*/
+    long long y = b;
+    long long answer = 0;
+    if (x<y){
+    while (x < y) {   /*x<y라면 x에는 +1 y에는 -1를 반복하면서 더하기*/
+        answer += x + y;
+        x++;
+        y--;
+    }
+}else if(x > y){
+    while(x > y){       /*x>y라면 x에는 -1 y에는 +1를 반복하면서 더하기*/
+        answer += x + y;
+        x--;
+        y++;
+    }
+}
+    if (x == y) answer += x; /*만약 같다면 그냥x를 출력*/
+
+    return answer;
 }
