741a97a5584966926d4c9022e7a01d47cbc0dd4a
gussen33 <gussen1221@gmail.com>
2025-07-04 09:19:03 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.cpp
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.cpp
@@ -5,11 +5,13 @@ using namespace std;
 
 string solution(int num) {
     string answer = "";
-       if(num%2==0){
-  answer = "Even";
-    } else{
-     answer = "Odd";
+    if(num%2==0)
+    {
+        answer="Even";
+    }else if(num%2!=0)
+    {
+        answer="Odd";
     }
-
+    
     return answer;
 }
