9de86dcb4b0fc79e1de695c916617828344a4adc
gussen33 <gussen1221@gmail.com>
2025-07-09 12:42:52 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 3.88 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -1,16 +1,17 @@
 #include <string>
 #include <vector>
-#include <cmath>
 
 using namespace std;
 
 long long solution(long long n) {
     long long answer = 0;
-    long long x=sqrt(n);
-    if(x*x!=n)
-        answer=-1;
-        else{
-            answer=(x+1)*(x+1);
+    answer=-1;
+    for(long long i = 1 ; i*i<=n; i++)
+    {
+        if(i*i==n)
+        {
+            answer=(i+1)*(i+1);
         }
+    }
     return answer;
 }
