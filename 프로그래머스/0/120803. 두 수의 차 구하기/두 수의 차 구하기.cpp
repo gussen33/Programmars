ea5b0d16f862287194142335305388a4ea2bfaad
gussen33 <gussen1221@gmail.com>
2026-01-15 09:29:00 +0900

    [level 0] Title: 두 수의 차 구하기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/0/120803. 두 수의 차 구하기/두 수의 차 구하기.cpp
+++ 프로그래머스/0/120803. 두 수의 차 구하기/두 수의 차 구하기.cpp
@@ -3,8 +3,11 @@
 
 using namespace std;
 
-int solution(int num1, int num2) {
+int solution(int num1, int num2) 
+{
     int answer = 0;
-    answer = num1-num2;
+    
+    answer= num1-num2;
+    
     return answer;
 }
