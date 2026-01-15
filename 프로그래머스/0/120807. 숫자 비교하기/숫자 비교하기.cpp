783d791916588a7e628637fd0a93866612f27885
gussen33 <gussen1221@gmail.com>
2026-01-15 09:32:34 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.cpp
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.cpp
@@ -3,13 +3,19 @@
 
 using namespace std;
 
-int solution(int num1, int num2) {
+int solution(int num1, int num2)
+{
     int answer = 0;
-    if(num1==num2){
-       answer=1;  
+    
+    if(num1==num2)
+    {
+        answer=1;
+    }else 
+    {
+        answer=-1;
     }
-  else {
-      answer=-1;}
+    
 
+    
     return answer;
 }
