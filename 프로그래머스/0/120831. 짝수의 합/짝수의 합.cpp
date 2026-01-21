53f4cb004ec9cc88832c450bf39fc329cb352e2e
gussen33 <gussen1221@gmail.com>
2026-01-21 10:43:27 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.cpp
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.cpp
@@ -5,12 +5,13 @@ using namespace std;
 
 int solution(int n) {
     int answer = 0;
-    for(int i = 1 ; i <=n ; i++)
-    {
-        if(i%2==0)
-        {
-            answer+=i;
-        }
+    
+    for(int i = 1 ; i<=n; i++)
+    {       
+       if(i%2==0)
+       {
+           answer+=i;
+       }
     }
     return answer;
 }
